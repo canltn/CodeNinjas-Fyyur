cce299c9e61e8e6e34ad2bc8f4d8a2b387d5c7c1
canltn <lethingoccanst@gmail.com>
2023-03-07 06:59:39 +0000

    connect database

--- app.py
+++ app.py
@@ -22,30 +22,42 @@ moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-# TODO: connect to a local postgresql database
+# Connect to a local postgresql database
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
+shows = db.Table('Shows',
+  db.Column('id', db.Integer, primary_key=True, autoincrement=True),
+  db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id')),
+  db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'))
+)
+
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(500))
+    website_link = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(1200))
+    artists = db.relationship("Artist", secondary=shows, backref=db.backref("venues", lazy=True))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+# TODO: implement any missing fields, as a database migration using Flask-Migrate #
+
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
@@ -53,10 +65,14 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(1200))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+# TODO: implement any missing fields, as a database migration using Flask-Migrate #
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.#
 
 #----------------------------------------------------------------------------#
 # Filters.
